e6cab8657193b2768fa8d5adc04ac331edda668b
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2022-07-05 12:02:42 -0300

    feat: add requirement 5 and 6

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
+const fs = require('fs').promises;
 const readFile = require('./helpers/readFile');
 const loginValidation = require('./helpers/loginValidation');
+const { tokenValidation,
+  nameValidation,
+  ageValidation,
+  talkValidation,
+  rateValidation,
+  watchedDateValidation } = require('./helpers/postValidation');
 
 const app = express();
 app.use(bodyParser.json());
@@ -41,3 +48,48 @@ app.post('/login', loginValidation, (req, res) => {
 
   return res.status(200).json({ token });
 });
+
+app.post('/talker',
+  tokenValidation,
+  nameValidation,
+  ageValidation,
+  talkValidation,
+  rateValidation,
+  watchedDateValidation,
+  async (req, res) => {
+    const { name, age, talk } = req.body;
+
+    const talkers = await readFile();
+    const newTalker = {
+      id: talkers.length + 1,
+      name,
+      age,
+      talk,
+    };
+
+    talkers.push(newTalker);
+    await fs.writeFile('./talker.json', JSON.stringify(talkers)); 
+    res.status(201).json(newTalker);
+  });
+
+  app.put(
+    '/talker/:id',
+    tokenValidation,
+    nameValidation,
+    ageValidation,
+    talkValidation,
+    watchedDateValidation,
+    rateValidation,
+    async (req, res) => {
+      const { id } = req.params;
+      const { name, age, talk } = req.body;
+      const talkers = await readFile();
+      const index = talkers.findIndex((talker) => talker.id === Number(id));
+      if (index === -1) {
+        return res.status(404).json({ message: 'Talker not found' });
+      }
+      talkers[index] = { ...talkers[index], name, age, talk };
+      await fs.writeFile('./talker.json', JSON.stringify(talkers));
+      res.status(200).json({ id: Number(id), name, age, talk });
+    },
+  );
